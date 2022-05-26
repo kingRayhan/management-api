19cfe78ef31d6f8f0fcfb18716daff2cbd204f3f
MIShuvro <manjarulislam75@gmail.com>
2022-05-26 11:21:19 +0600

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './api/user/user.module';
+import { AuthModule } from './api/auth/auth.module';
+import { RoleModule } from './api/role/role.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev', '.env.prod'],
+      isGlobal: true,
+    }),
+    // AuthModule,
+   // UserModule,
+  //  RoleModule
+  ],
   controllers: [AppController],
   providers: [],
 })
