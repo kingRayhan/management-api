2e67e302464fbe79f003e450d726c7c13ce3762c
King Rayhan <rayhan095@gmail.com>
2022-06-01 11:16:11 +0600

    remove /video/[identifier] page

--- src/api/article/article.module.ts
+++ src/api/article/article.module.ts
@@ -3,10 +3,8 @@ import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Article } from '@/api/article/entities/article.entity';
-import { UserModule } from '@/api/user/user.module';
 import { User } from '@/api/user/entities/user.entity';
 import { RoleModule } from '@/api/role/role.module';
-import { ArticlePolicy } from './article.policy';
 
 @Module({
   imports: [TypegooseModule.forFeature([Article, User]), RoleModule],
